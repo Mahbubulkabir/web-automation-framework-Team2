db0cde8649f261f7d89ca1e813b8f708881bdc88
Mahbubulkabir <munnagajaghanta@Gmail.com>
2023-02-07 02:11:25 -0500

    test commit

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,2 +1,29 @@
-package pages;public class HomePage {
+package pages;
+
+
+import base.CommonAPI;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage extends CommonAPI {
+
+   Logger LOG = LogManager.getLogger(HomePage.class.getName());
+    public HomePage(WebDriver driver){
+        PageFactory.initElements(driver,this);
+    }
+
+    // object(web element)
+    @FindBy(xpath = "")
+    WebElement element;
+
+    // reusable method
+  public void setElement(){
+      clickOn(element);
+  }
+
+
 }
